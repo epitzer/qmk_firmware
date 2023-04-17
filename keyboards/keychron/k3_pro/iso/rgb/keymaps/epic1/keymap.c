b1603fe2c18cd2db277292b180ac9b4d4f2098e7
Erik Pitzer <erik.pitzer@fh-hagenberg.at>
2023-04-17 11:51:41 +0200

    add grave character to strict shift/control sides

--- keyboards/keychron/k3_pro/iso/rgb/keymaps/epic1/keymap.c
+++ keyboards/keychron/k3_pro/iso/rgb/keymaps/epic1/keymap.c
@@ -157,7 +157,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if ((strict_shift && !is_caps_word_on()) || strict_control) {
       if (left_shift_active || left_control_active) {
           switch (keycode) {
-          case KC_1: case KC_2: case KC_3: case KC_4:
+          case KC_GRV: case KC_1: case KC_2: case KC_3: case KC_4:
           case KC_Q: case KC_W: case KC_E: case KC_R:
           case KC_A: case KC_S: case KC_D: case KC_F:
           case KC_Z: case KC_X: case KC_C: case KC_V:
